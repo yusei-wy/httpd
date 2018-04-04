66e6883343fc4500959ad85ae05f74d7600626da
yusei-wy <yusei.kasa@gmail.com>
2018-04-05 07:26:51 +0900

    Add httpd サーバを作成

--- httpd.c
+++ httpd.c
@@ -5,14 +5,20 @@
 #include <ctype.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <getopt.h>
+#include <grp.h>
+#include <netdb.h>
+#include <pwd.h>
 #include <signal.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <strings.h>
+#include <sys/socket.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <syslog.h>
 #include <time.h>
 #include <unistd.h>
 
@@ -22,6 +28,19 @@
 #define BLOCK_BUF_SIZE 1024
 #define LINE_BUF_SIZE 4096
 #define MAX_REQUEST_BODY_LENGTH (1024 * 1024)
+#define USAGE "Usage: %s [--port=n] [--chroot --user=u --group=u] <docroot>\n"
+
+static int debug_mode = 0;
+
+static struct option longopts[] = {
+    {"debug", no_argument, &debug_mode, 1},
+    {"chroot", no_argument, NULL, 'c'},
+    {"user", required_argument, NULL, 'u'},
+    {"group", required_argument, NULL, 'g'},
+    {"port", required_argument, NULL, 'p'},
+    {"help", no_argument, NULL, 'h'},
+    {0, 0, 0, 0},
+};
 
 struct HTTPHeaderField {
   char *name;
@@ -46,7 +65,7 @@ struct FileInfo {
 
 typedef void (*sighandler_t)(int);
 
-static void log_exit(char *, ...);
+static void log_exit(const char *, ...);
 static void *xmalloc(size_t);
 static void install_signal_handlers(void);
 static void trap_signal(int, sighandler_t);
@@ -69,15 +88,63 @@ static void method_not_allowed(struct HTTPRequest *, FILE *);
 static void not_implemented(struct HTTPRequest *, FILE *);
 static void not_found(struct HTTPRequest *, FILE *);
 static void output_common_header_files(struct HTTPRequest *, FILE *, char *);
+static int listen_socket(char *);
+static void server_main(int, char *);
+static void detach_children(void);
+static void noop_handler(int);
+static void become_daemon(void);
+static void setup_environment(char *, char *, char *);
 
 int main(int argc, char *argv[]) {
-  if (argc != 2) {
-    fprintf(stderr, "Usage: %s <docroot>\n", argv[0]);
+  int server_fd;
+  char *port = NULL;
+  char *docroot;
+  int do_chroot = 0;
+  char *user = NULL;
+  char *group = NULL;
+  int opt;
+
+  while ((opt = getopt_long(argc, argv, "", longopts, NULL)) != -1) {
+    switch (opt) {
+    case 0:
+      break;
+    case 'c':
+      do_chroot = 1;
+      break;
+    case 'u':
+      user = optarg;
+      break;
+    case 'g':
+      group = optarg;
+      break;
+    case 'p':
+      port = optarg;
+      break;
+    case 'h':
+      fprintf(stdout, USAGE, argv[0]);
+      exit(0);
+    case '?':
+      fprintf(stderr, USAGE, argv[0]);
+      exit(1);
+    }
+  }
+  if (optind != argc - 1) {
+    fprintf(stderr, USAGE, argv[0]);
     exit(1);
   }
-  // TODO: docroot がディレクトリならエラー
+  docroot = argv[optind];
+
+  if (do_chroot) {
+    setup_environment(docroot, user, group);
+    docroot = "";
+  }
   install_signal_handlers();
-  service(stdin, stdout, argv[1]);
+  server_fd = listen_socket(port);
+  if (!debug_mode) {
+    openlog(SERVER_NAME, LOG_PID | LOG_NDELAY, LOG_DAEMON);
+    become_daemon();
+  }
+  server_main(server_fd, docroot);
   exit(0);
 }
 
@@ -85,12 +152,16 @@ int main(int argc, char *argv[]) {
  * printf() と同じ形式で引数を受けて
  * それをフォーマットしたものを標準エラー出力に出力する
  */
-static void log_exit(char *fmt, ...) {
+static void log_exit(const char *fmt, ...) {
   va_list ap;
 
   va_start(ap, fmt);
-  vfprintf(stderr, fmt, ap);
-  fputc('\n', stderr);
+  if (debug_mode) {
+    vfprintf(stderr, fmt, ap);
+    fputc('\n', stderr);
+  } else {
+    vsyslog(LOG_ERR, fmt, ap);
+  }
   va_end(ap);
   exit(1);
 }
@@ -110,7 +181,10 @@ static void *xmalloc(size_t sz) {
 /**
  * シグナル SIGPIPE を補足したら exit()
  */
-static void install_signal_handlers(void) { trap_signal(SIGPIPE, signal_exit); }
+static void install_signal_handlers(void) {
+  trap_signal(SIGPIPE, signal_exit);
+  detach_children();
+}
 
 /**
  * シグナルの補足
@@ -472,4 +546,148 @@ static void output_common_header_files(struct HTTPRequest *req, FILE *out,
   fprintf(out, "Date: %s\r\n", buf);
   fprintf(out, "Server: %s/%s\r\n", SERVER_NAME, SERVER_VERSION);
   fprintf(out, "Connection: close\r\n");
+}
+
+#define MAX_BACKLOG 5
+#define DEFAULT_PORT "80"
+
+/**
+ * socket() から listen() までを担当
+ */
+static int listen_socket(char *port) {
+  struct addrinfo hints, *res, *ai;
+  int err;
+
+  memset(&hints, 0, sizeof(struct addrinfo));
+  hints.ai_family = AF_INET;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE;
+  if ((err = getaddrinfo(NULL, port, &hints, &res)) != 0)
+    log_exit(gai_strerror(err));
+  for (ai = res; ai; ai = ai->ai_next) {
+    int sock;
+
+    sock = socket(ai->ai_family, ai->ai_socktype, ai->ai_protocol);
+    if (sock < 0)
+      continue;
+    if (bind(sock, ai->ai_addr, ai->ai_addrlen) < 0) {
+      close(sock);
+      continue;
+    }
+    if (listen(sock, MAX_BACKLOG) < 0) {
+      close(sock);
+      continue;
+    }
+    freeaddrinfo(res);
+    return sock;
+  }
+  log_exit("failed to listen socket");
+  return -1; // NOT REACH
+}
+
+/**
+ * サーバーのメイン処理
+ */
+static void server_main(int server_fd, char *docroot) {
+  for (;;) {
+    struct sockaddr_storage addr;
+    socklen_t addrlen = sizeof addr;
+    int sock;
+    int pid;
+
+    sock = accept(server_fd, (struct sockaddr *)&addr, &addrlen);
+    if (sock < 0)
+      log_exit("accept(2) failed: %s", strerror(errno));
+    pid = fork();
+    if (pid < 0)
+      exit(3);
+    if (pid == 0) { // child
+      FILE *inf = fdopen(sock, "r");
+      FILE *outf = fdopen(sock, "w");
+
+      service(inf, outf, docroot);
+      exit(0);
+    }
+    close(sock);
+  }
+}
+
+/**
+ * sigaction() を使って wait() しないことをカーネルに宣言
+ */
+static void detach_children(void) {
+  struct sigaction act;
+
+  act.sa_handler = noop_handler;
+  sigemptyset(&act.sa_mask);
+  act.sa_flags = SA_RESTART | SA_NOCLDWAIT;
+  if (sigaction(SIGCHLD, &act, NULL) < 0)
+    log_exit("sigaction() failed: %s", strerror(errno));
+}
+
+/**
+ * SIGCHILD のシグナルハンドラ, なにもしない
+ */
+static void noop_handler(int sig) { ; }
+
+/**
+ * デーモンになる
+ */
+static void become_daemon(void) {
+  int n;
+
+  if (chdir("/") < 0)
+    log_exit("child(2) failed: %s", strerror(errno));
+  freopen("/dev/null", "r", stdin);
+  freopen("/dev/null", "w", stdout);
+  freopen("/dev/null", "w", stderr);
+  n = fork();
+  if (n < 0)
+    log_exit("fork(2) failed: %s", strerror(errno));
+  if (n != 0) // 親プロセスは終了
+    _exit(0);
+  if (setsid() < 0)
+    log_exit("setsid(2) failed: %s", strerror(errno));
+}
+
+/**
+ * 環境をセットアップする. chroot() はスーパユーザでしか実行できないので
+ * 実行前にクレデンシャルを事前に変更する.
+ */
+static void setup_environment(char *root, char *user, char *group) {
+  struct passwd *pw;
+  struct group *gr;
+
+  if (!user || !group) {
+    fprintf(stderr, "use both of --user and --group\n");
+    exit(1);
+  }
+
+  gr = getgrnam(group);
+  if (!gr) {
+    fprintf(stderr, "use both of --user and --group\n");
+    exit(1);
+  }
+
+  if (setgid(gr->gr_gid) < 0) {
+    perror("setgid(2)");
+    exit(1);
+  }
+
+  if (initgroups(user, gr->gr_gid) < 0) {
+    perror("initgroups(2)");
+    exit(1);
+  }
+
+  pw = getpwnam(user);
+  if (!pw) {
+    fprintf(stderr, "no such user: %s\n", user);
+    exit(1);
+  }
+
+  chroot(root);
+  if (setuid(pw->pw_uid) < 0) {
+    perror("setuid(2)");
+    exit(1);
+  }
 }
